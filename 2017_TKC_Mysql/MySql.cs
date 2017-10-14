66464cf70b88c104ebc1f8022e1a4aca80466de5
jpcaradec <32782004+jpcaradec@users.noreply.github.com>
2017-10-14 09:33:11 -0300

    Criada Namesapece MySQl
    Namespace Mysql:
    Classe BancodeDados

---
+++ 2017_TKC_Mysql/MySql.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySql
+{
+    class BancodeDados
+    {
+        private string IP;
+        private string Usuario;
+        private string Senha;
+        private string BD;
+
+        private bool VerificaBD(string ip, string usuario, string senha)
+        {
+            
+            return true;
+        }
+        private bool VerificaBD(string ip, string usuario, string senha, string bd)
+        {
+
+            return true;
+        }
+    }
+}
