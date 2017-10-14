449c26d1321a0740013467b9941d59e9a61ef2c8
jpcaradec <32782004+jpcaradec@users.noreply.github.com>
2017-10-14 09:59:02 -0300

    Alteração
    Eu tinha criado uma classe Servdiodr dentro da Classe Mysql, aí lembrei de usar o Servidor como SIngleton. Removi a classe Servidor da classe MySql

--- 2017_TKC_Mysql/MySql.cs
+++ 2017_TKC_Mysql/MySql.cs
@@ -3,33 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Singleton;
 
 namespace MySql
 {
     class BancodeDados
     {
-        class Servidor
-        {
-            private string IP;
-            private string Usuario;
-            private string Senha;
-        }
         
-        //Verifica acesso ao Servidor
-        private bool VerificaBD(string ip, string usuario, string senha)
+        
+        //Verifica acesso ao Servidor (Manespace Singleton)
+        private bool VerificaBD(Servidor servidor)
         {
             return true;
         }
 
-        //Verifica se Servidor tem o Banco de Dados (bd)
-        private bool VerificaBD(string ip, string usuario, string senha, string bd)
+        //Verifica se Servidor tem o Banco de Dados (Manespace Singleton Servidor + bd)
+        private bool VerificaBD(Servidor servidor, string bd)
         {
 
             return true;
         }
 
         //Cria bd no Servidor
-        private bool CriaBd(Servidor servidor)
+        private bool CriaBd(Servidor servidor, string bd)
         {
             return true;
         }
